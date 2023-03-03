5776d483b4eaf62654bfce7ca9a2cdecc1ca0a88
iuliiast <40248982+iuliiast@users.noreply.github.com>
2023-03-03 23:06:37 +0400

    fix ts

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,10 +1,12 @@
+import "dotenv/config";
 import cors from "cors";
 import express from "express";
 import ViteExpress from "vite-express";
 import { routerIndex } from "../../routers/router.index";
 const app = express();
-app.use(cors());
+const port: any = process.env.PORT || 3000;
 
+app.use(cors());
 app.get("/hello", (_, res) => {
 	res.send("Hello Vite + React + TypeScript!");
 });
@@ -16,6 +18,6 @@ app.use("/data", express.static("./data"));
 app.use("/images", express.static("./images"));
 app.use("/", routerIndex);
 
-ViteExpress.listen(app, 3000, () =>
-	console.log("Server is listening on port 3000...")
+ViteExpress.listen(app, port, () =>
+	console.log(`Server is listening on port ${port}...`)
 );
